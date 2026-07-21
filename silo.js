b0d4cc9dc231287149f85794f2d26a2e76ccd562
helmismd <helmi.2573er@gmail.com>
2026-07-21 14:30:59 +0800

    Add files via upload

---
+++ silo.js
@@ -0,0 +1,22 @@
+//const level = document.getElementById("level1");
+//
+//level.style.height = "10%";
+//
+const stok = 5000;
+const kapasitas = 6000;
+
+const persen = (stok / kapasitas) * 100;
+
+level.style.height = persen + "%";//
+//
+
+fetch("data.json")
+    .then(res => res.json())
+    .then(data => {
+
+        console.log(data);
+
+    });
+
+
+
