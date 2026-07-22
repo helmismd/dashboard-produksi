c567887a5bd128d257d64a9ba26aa47c2b82f8b5
helmismd <helmi.2573er@gmail.com>
2026-07-22 14:29:02 +0800

    Add files via upload

--- silo.js
+++ silo.js
@@ -9,12 +9,13 @@ fetch("data.json")
     document.getElementById("lastUpdate").textContent =
         data.lastUpdate;
 
-    // ==========================
-    // KONSTANTA
-    // ==========================
+   // ==========================
+   // KONSTANTA
+   // ==========================
 
-    const kapasitas = 6000;
-    const deadStock = 200;
+	const kapasitas = 6000;
+	const deadStock = 200;
+	const batasKuning = 50;
 
     // ==========================
     // DATA OPC
@@ -51,6 +52,44 @@ fetch("data.json")
     document.getElementById("levelOPC").style.height =
         persenOPC + "%";
 
+// Warna Level OPC
+if (stokOPC <= deadStock) {
+
+    document.getElementById("levelOPC").style.background = "#d60000";
+
+}
+else if (persenOPC <= batasKuning) {
+
+    document.getElementById("levelOPC").style.background = "#ffd400";
+
+}
+else {
+
+    document.getElementById("levelOPC").style.background = "#00a651";
+
+}
+
+// Status OPC
+const statusOPC = document.getElementById("statusOPC");
+
+if (stokOPC <= deadStock) {
+
+    statusOPC.textContent = "KRITIS";
+    statusOPC.style.color = "#d60000";
+
+}
+else if (persenOPC <= batasKuning) {
+
+    statusOPC.textContent = "WASPADA";
+    statusOPC.style.color = "#ffd400";
+
+}
+else {
+
+    statusOPC.textContent = "NORMAL";
+    statusOPC.style.color = "#00a651";
+
+}
     // ==========================
     // TAMPILKAN PCC
     // ==========================
@@ -63,6 +102,45 @@ fetch("data.json")
 
     document.getElementById("levelPCC").style.height =
         persenPCC + "%";
+
+// Warna Level PCC
+if (stokPCC <= deadStock) {
+
+    document.getElementById("levelPCC").style.background = "#d60000";
+
+}
+else if (persenPCC <= batasKuning) {
+
+    document.getElementById("levelPCC").style.background = "#ffd400";
+
+}
+else {
+
+    document.getElementById("levelPCC").style.background = "#00a651";
+
+}
+
+// Status PCC
+const statusPCC = document.getElementById("statusPCC");
+
+if (stokPCC <= deadStock) {
+
+    statusPCC.textContent = "KRITIS";
+    statusPCC.style.color = "#d60000";
+
+}
+else if (persenPCC <= batasKuning) {
+
+    statusPCC.textContent = "WASPADA";
+    statusPCC.style.color = "#ffd400";
+
+}
+else {
+
+    statusPCC.textContent = "NORMAL";
+    statusPCC.style.color = "#00a651";
+
+}
 
 })
 .catch(err => {
