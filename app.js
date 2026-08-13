af13f70952ef61ff1eeb7601aae4d36b6fc6a7ac
helmismd <helmi.2573er@gmail.com>
2026-08-13 18:57:38 +0800

    Add files via upload

--- app.js
+++ app.js
@@ -1282,7 +1282,10 @@ console.log("STATUS KAPAL FINAL =", statusKapal);
         opc: kapal.opc || "0",
         pcc: kapal.pcc || "0",
         total: kapal.total || "0",
-        statusKapal: statusKapal || kapal.status || "-",
+        statusKapal:
+    (kapal.status && kapal.status !== "-")
+        ? kapal.status
+        : (statusKapal || "-"),
         statusOperasi: statusOperasi, // Sudah dikunci aman ke "Lanjut Bongkar"
         update: terakhir ? terakhir.datetime : ""
     };
